6135adf86bc9d7e493aa12f4a84b6eef8975ffe4
Shyamal <patelshyamal016@gmail.com>
2024-09-13 14:26:03 -0400

    Shader calculation

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -7,6 +7,7 @@ layout(location=1) in vec4 aColor;
 uniform mat4 uProjection;
 uniform mat4 uView;
 
+
 out vec4 fColor;
 
 void main(){
@@ -17,9 +18,13 @@ void main(){
 #type fragment
 #version 330 core
 
+uniform float uTime;
+
 in vec4 fColor;
 out vec4 color;
 
 void main(){
-    color = fColor;
+//  Generate a noise based on random seeds (dot)
+    float noise = fract(sin(dot(fColor.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    color = fColor * noise;
 }
