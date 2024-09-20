c28101210c2b2ba43eff655abb944ea194476738
Shyamal <patelshyamal016@gmail.com>
2024-09-20 18:20:11 -0400

    Fixed a bug where the game would crash if there were no objects in scene

--- src/main/java/scenes/LevelEditorScene.java
+++ src/main/java/scenes/LevelEditorScene.java
@@ -2,65 +2,78 @@ package scenes;
 
 
 import Core.*;
+import Physics2D.PhysicsSystem2D;
+import Physics2D.rigidbody.Rigidbody2D;
 import components.*;
 import imgui.ImGui;
 import imgui.ImVec2;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
-import org.joml.Vector4f;
-import org.lwjgl.system.CallbackI;
 import renderer.DebugDraw;
 import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
-
-    private GameObject obj1, obj2;
     private Spritesheet sprites;
-    private SpriteRenderer obj1Sprite;
 
     GameObject levelEditorStuff = new GameObject("LevelEditor", new Transform(new Vector2f()), 0);
+    PhysicsSystem2D physics = new PhysicsSystem2D(1.0f/60.f, new Vector2f(0, -10));
+    Transform obj1, obj2;
+    Rigidbody2D rb1, rb2;
 
     public LevelEditorScene(){
 
     }
+
     @Override
     public void init() {
         levelEditorStuff.addComponent(new MouseControls());
         levelEditorStuff.addComponent(new GridLines());
+
+        obj1 = new Transform(new Vector2f(100,500));
+        obj2 = new Transform(new Vector2f(200,500));
+        rb1 = new Rigidbody2D();
+        rb2 = new Rigidbody2D();
+
+        rb1.setRawTransform(obj1);
+        rb2.setRawTransform(obj2);
+
+        rb1.setMass(100.0f);
+        rb2.setMass(200.0f);
+
+        physics.addRigidbody(rb1);
+        physics.addRigidbody(rb2);
+
         loadResources();
         this.camera = new Camera(new Vector2f(new Vector2f(-250, 0)));
         sprites = AssetPool.getSpritesheet("D:\\GameEngine\\assets\\images\\decorationsAndBlocks.png");
+//        if(levelLoaded){
+//            this.activeGameObject = gameObjects.get(0);
+//            this.activeGameObject.addComponent(new Rigidbody());
+//            return;
+//        }
+
         if(levelLoaded){
-            this.activeGameObject = gameObjects.get(0);
-            this.activeGameObject.addComponent(new Rigidbody());
+            if(gameObjects.size() > 0){
+                this.activeGameObject = gameObjects.get(0);
+            }
             return;
         }
 
-        sprites = AssetPool.getSpritesheet("D:\\GameEngine\\assets\\images\\decorationsAndBlocks.png");
-
-//        obj1 = new GameObject("Obj1", new Transform(new Vector2f(600, 100), new Vector2f(128, 128)), 2);
-////        obj1.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("D:\\GameEngine\\assets\\images\\1.png"))));
-//        obj1Sprite = new SpriteRenderer();
-//        obj1Sprite.setColor(new Vector4f(0,1,0,1));
-//        obj1.addComponent(obj1Sprite);
-//        obj1.addComponent(new Rigidbody());
-//        this.addGameObjectToScene(obj1);
-//        this.activeGameObject = obj1;
-//        obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
-
-
-//        obj2 = new GameObject("Obj2", new Transform(new Vector2f(300, 100), new Vector2f(128, 128)), 1);
-//        SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
-//        Sprite obj2Sprite = new Sprite();
-//        obj2Sprite.setTexture(AssetPool.getTexture("D:\\GameEngine\\assets\\images\\LanceRunningAnimation.png"));
-//        obj2SpriteRenderer.setSprite(obj2Sprite);
-//        obj2.addComponent(obj2SpriteRenderer);
-//        this.addGameObjectToScene(obj2);
+//        sprites = AssetPool.getSpritesheet("D:\\GameEngine\\assets\\images\\decorationsAndBlocks.png");
     }
 
     private void loadResources(){
         AssetPool.getShader("D:\\GameEngine\\assets\\shaders\\default.glsl");
         AssetPool.addSpriteSheet("D:\\GameEngine\\assets\\images\\decorationsAndBlocks.png", new Spritesheet(AssetPool.getTexture("D:\\GameEngine\\assets\\images\\decorationsAndBlocks.png"), 16, 16, 81, 0 ));
+
+//        for(GameObject go : gameObjects){
+//            if(go.getComponent(SpriteRenderer.class) != null){
+//                SpriteRenderer spr = go.getComponent(SpriteRenderer.class);
+//                if(spr.getTexture() != null){
+//                    spr.setTexture(AssetPool.getTexture(spr.getTexture().getFilePath()));
+//                }
+//            }
+//        }
     }
 
     float x = 0.0f;
@@ -69,13 +82,15 @@ public class LevelEditorScene extends Scene {
     public void update(float dt) {
 //        System.out.println("FPS " + (1.0f/dt));
         levelEditorStuff.update(dt);
-        DebugDraw.addCircle2D(new Vector2f(x, y), 64, new Vector3f(0, 1, 0), 1);
-        x+=50.0f*dt;
-        y+=50.0f*dt;
+
         for(GameObject go : this.gameObjects){
             go.update(dt);
         }
 
+        DebugDraw.addBox2D(obj1.position, new Vector2f(32,32), 30.0f, new Vector3f(1,0,0));
+        DebugDraw.addBox2D(obj2.position, new Vector2f(32,32), 0.0f, new Vector3f(0,1,0));
+
+        physics.update(dt);
         this.renderer.render();
     }
 
